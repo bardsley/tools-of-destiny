ee6ae58060a7463189651f1b904e7d7882b9a1fe
Adam Bardsley <adam.bardsley@gmail.com>
2022-10-30 11:28:25 +0000

    Adding raid dad

--- javascript/main.js
+++ javascript/main.js
@@ -1,62 +1,91 @@
 var apps = [
         {   "name": "Braytech",                 "url": "https://bray.tech",
-            "description": "", "tags": [ "maps","bounties","quests","vendors","triumphs","collections","weeklies","stats","lore" ]
+            "description": "", "tags": [ "maps","bounties","quests","vendors","triumphs","collections","weeklies","stats","lore" ],
+            "primary-colour": "#555", "secondary-colour": "#ccc", "logo": "", "text-colour": "#fff"
         },{ "name": "Destiny recipes",          "url": "https://destinyrecipes.com",
-            "description": "","tags":  [ "item-mgt","vault","weapons","armor","godrolls","power","prep" ]
+            "description": "","tags":  [ "item-mgt","vault","weapons","armor","godrolls","power","prep" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "DIM",                      "url": "https://app.destinyitemmanager.com",
-            "description": "", "tags": [ "bounties","quests","vendors","triumphs","collections","weeklies","item-mgt","vault","loadouts","weapons","armor","godrolls","power"]
+            "description": "", "tags": [ "bounties","quests","vendors","triumphs","collections","weeklies","item-mgt","vault","loadouts","weapons","armor","godrolls","power"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "D2 Checklist",             "url": "https://www.d2checklist.com",
-            "description": "", "tags": [ "bounties","quests","vendors","triumphs","collections","weeklies","power","prep" ]
+            "description": "", "tags": [ "bounties","quests","vendors","triumphs","collections","weeklies","power","prep" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Shattered Vault",          "url": "https://shatteredvault.com",
-            "description": "", "tags": [ "guides","lore" ]
+            "description": "", "tags": [ "guides","lore" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Xur",                      "url": "https://wherexur.com",
-            "description": "", "tags": [ "vendors","weapons","armor","stats" ]
+            "description": "", "tags": [ "vendors","weapons","armor","stats" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Light.gg",                         "url": "https://www.light.gg",
-            "description": "", "tags": [ "item-mgt","vault","weapons","armor","godrolls"]
+            "description": "", "tags": [ "item-mgt","vault","weapons","armor","godrolls"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Destiny tracker",                         "url": "https://destinytracker.com",
-            "description": "", "tags": [ "stats","pvp","pve"]
+            "description": "", "tags": [ "stats","pvp","pve"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "D2 Gunsmith",                         "url": "https://d2gunsmith.com",
-            "description": "", "tags": [ "weapons","godrolls" ]
+            "description": "", "tags": [ "weapons","godrolls" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Destiny Sets",             "url": "https://destinysets.com/",
-            "description": "", "tags": [ "weapons","armor","stats" ]
+            "description": "", "tags": [ "weapons","armor","stats" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Guardian Forge",           "url": "https://guardianforge.net/",
-            "description": "", "tags": [ "loadouts","weapons","armor" ]
+            "description": "", "tags": [ "loadouts","weapons","armor" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Trials Report",            "url": "https://trials.report/",
-            "description": "", "tags": [ "stats","pvp" ]
+            "description": "", "tags": [ "stats","pvp" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Guardian Report",          "url": "https://guardian.report/",
-            "description": "", "tags": [ "stats","pve" ]
+            "description": "", "tags": [ "stats","pve" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Raid Report",              "url": "https://raid.report/",
-            "description": "", "tags": ["stats","raid" ]
+            "description": "", "tags": ["stats","raid" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Wasted on Destiny",        "url": "https://wastedondestiny.com/",
-            "description": "", "tags": [ "stats"]
+            "description": "", "tags": [ "stats"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Where is Xur?",            "url": "https://whereisxur.com/",
-            "description": "", "tags": [ "vendors","weapons","armor" ]
+            "description": "", "tags": [ "vendors","weapons","armor" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Destiny Heat Map",         "url": "https://chrisfried.github.io/secret-scrublandeux/",
-            "description": "", "tags": [ "stats" ]
+            "description": "", "tags": [ "stats" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Guardian Theatre",         "url": "https://alpha.guardian.theater/",
-            "description": "", "tags": [ "stats"]
+            "description": "", "tags": [ "stats"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Emblem Collector", "url": "https://destinyemblemcollector.com/",
-            "description": "", "tags": [ "collections" ]
+            "description": "", "tags": [ "collections" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Today in Destiny",         "url": "https://www.todayindestiny.com/",
-            "description": "", "tags": [ "bounties","quests","vendors","collections","weeklies" ]
+            "description": "", "tags": [ "bounties","quests","vendors","collections","weeklies" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Previously on Destiny",    "url": "https://www.previouslyondestiny.com/",
-            "description": "", "tags": [ "lore" ]
+            "description": "", "tags": [ "lore" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Destiny K/D",              "url": "https://www.destinykd.com/",
-            "description": "", "tags": [ "pvp","trials"]
+            "description": "", "tags": [ "pvp","trials"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Blueberries.gg",                         "url": "https://www.blueberries.gg/",
-            "description": "", "tags": [ "guides", "weapons", "armor" ]
+            "description": "", "tags": [ "guides", "weapons", "armor" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "D2 Armor Picker",                         "url": "https://d2armorpicker.com/#/",
-            "description": "", "tags": [ "loadouts", "armor" ]
+            "description": "", "tags": [ "loadouts", "armor" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Nightfall Report",                         "url": "https://nightfall.report",
-            "description": "", "tags": [ "stats","pve" ]
+            "description": "", "tags": [ "stats","pve" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Bungie Twitter",           "url": "https://twitter.com/BungieHelp",
-            "description": "", "tags": [ "stats" ]
+            "description": "", "tags": [ "stats" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Memento Viewer",                         "url": "https://altbdoor.github.io/d2-memento/",
-            "description": "", "tags": [ "looks" ]
+            "description": "", "tags": [ "looks" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "Destiny Symbols",                         "url": "https://nathanrsxtn.github.io/Destiny-2-Font-Symbols/",
-            "description": "", "tags": [ "looks" ]
-        },{ "name": "",                         "url": "",
-            "description": "", "tags": [ ]
+            "description": "", "tags": [ "looks" ],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
+        },{ "name": "Raid Dad",                         "url": "https://www.raiddad.com/clan/3/3859771?players[0]=4611686018484097098&playerPlatforms[0]=3",
+            "description": "Basic toool (a little old) showing your clan/fireteams loadouts", "tags": ["raid"],
+            "primary-colour": "#", "secondary-colour": "#", "logo": "", "text-colour": "#"
         },{ "name": "",                         "url": "",
             "description": "", "tags": [ ]
         },{ "name": "",                         "url": "",
@@ -139,10 +168,14 @@ var apps = [
             if(app.name && app.url) {
                 var clone = appTemplate.content.firstElementChild.cloneNode(true);
                 clone.className = clone.className + " " + app.tags.join(' ')
+                var title = clone.querySelector(".title")
                 clone.querySelector(".title").innerText = app.name
+                if(app["primary-colour"]) { title.style["background-color"] = app["primary-colour"] }
+                if(app["secondary-colour"]) { clone.style["background-color"] = app["secondary-colour"] }
+
                 if(app.description) { clone.querySelector(".desc").innerText = app.description } else { clone.querySelector(".desc").remove() }
                 if(app.tags && app.tags.length >0) { 
-                    clone.querySelector(".tags").innerHTML = app.tags.sort().map(tag=>{ return `<span data-tag="${tag}" class="tag ${tag}">${tag}</span>` }).join(' ') 
+                    clone.querySelector(".tags").innerHTML = app.tags.sort().map(tag=>{ return `<span data-tag="${tag}" style="background-color: ${app["primary-colour"]}" class="tag ${tag}">${tag}</span>` }).join(' ') 
                 } else { clone.querySelector(".tags").remove() }
                 clone.querySelector(".button").href = app.url
                 appSpace.append(clone);
