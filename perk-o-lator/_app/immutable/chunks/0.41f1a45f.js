957c9cdae2266db254007f8d929b65eb0c4e3582
Adam Bardsley <adam.bardsley@gmail.com>
2023-03-28 23:12:34 +0100

    Adding first protoype of perk-o-lator

---
+++ perk-o-lator/_app/immutable/chunks/0.41f1a45f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout.0229455e.js";import{default as t}from"../entry/_layout.svelte.a207f5ef.js";export{t as component,r as universal};
---
+++ perk-o-lator/_app/immutable/chunks/2.102dc2f7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.728d6a2e.js";export{t as component};
